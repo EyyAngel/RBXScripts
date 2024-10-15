688136c836fccbb53cf6cbff8f22901a6c0af046
EyyAngel <89284522+EyyAngel@users.noreply.github.com>
2024-10-15 15:33:30 -0700

    Update Main.lua

--- TDS/Main.lua
+++ TDS/Main.lua
@@ -1,1 +1,6 @@
+if not game:IsLoaded() then game.Loaded:Wait() end
 
+local TextServ = game:GetService("TextChatService")
+local general = TextServ:WaitForChild("TextChannels"):WaitForChild("RBXGeneral") :: TextChannel
+
+general:DisplaySystemMessage("Hello World!")
