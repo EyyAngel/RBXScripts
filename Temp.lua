8153960152bb17b88a15490dd9dbea3c650a95b3
EyyAngel <89284522+EyyAngel@users.noreply.github.com>
2024-10-17 12:58:56 -0700

    Update Temp.lua

--- Temp.lua
+++ Temp.lua
@@ -2,7 +2,7 @@ local TextServ = game:GetService("TextChatService")
 local generalChat = TextServ:WaitForChild("TextChannels"):WaitForChild("RBXGeneral") :: TextChannel
 
 SendMessage = function(message)
-    -- message = `<font size="20" color="#FF7000">{message}</font>`
+    message = `<font size="20" color="#FF7000">{message}</font>`
     generalChat:DisplaySystemMessage(message)
 end
 
