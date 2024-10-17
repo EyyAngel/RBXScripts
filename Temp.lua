bef647b9926b7b53bf253d2df2af72e04c832add
EyyAngel <89284522+EyyAngel@users.noreply.github.com>
2024-10-17 13:01:42 -0700

    Update Temp.lua

--- Temp.lua
+++ Temp.lua
@@ -1,9 +1,10 @@
 local TextServ = game:GetService("TextChatService")
 local generalChat = TextServ:WaitForChild("TextChannels"):WaitForChild("RBXGeneral") :: TextChannel
 
-SendMessage = function(message)
-    message = `<font size="20" color="#FF7000">{message}</font>`
+SendMessage = function(msg)
+    local message = `<font size="20" color="#FF7000">{msg}</font>`
     generalChat:DisplaySystemMessage(message)
 end
 
 SendMessage("Hello World")
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/EyyAngel/RBXScripts/refs/heads/main/Temp.lua"))()
