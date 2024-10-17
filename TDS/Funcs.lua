d38d08109fbbd3652dab6c12ff7700005a638e29
EyyAngel <89284522+EyyAngel@users.noreply.github.com>
2024-10-16 22:34:42 -0700

    Update Funcs.lua

--- TDS/Funcs.lua
+++ TDS/Funcs.lua
@@ -6,7 +6,9 @@ local generalChat = TextServ:WaitForChild("TextChannels"):WaitForChild("RBXGener
 local remoteFunc = game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunction") :: RemoteFunction
 local remoteEvent = game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvent") :: RemoteEvent
 
-local Functions = {}
+local Functions = {
+    lobbyId = 3260590327
+}
 
 Functions.SendMessage = function(message)
     generalChat:DisplaySystemMessage(message)
@@ -14,13 +16,14 @@ end
 
 Functions.PrivateMatch = function()
     remoteFunc:InvokeServer("Multiplayer", "v2:start", {
+        difficulty = "Easy",
         count = 1,
         mode = "survival"
     })
 end
 
 Functions.ReturnToLobby = function()
-    TPServ:Teleport(3260590327, player)
+    TPServ:Teleport(Functions.lobbyId, player)
 end
 
 return Functions
